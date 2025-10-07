f288c0d04bbc46954592b6c8d239d93e8f5312a8
rinsh4dd <rinshadcontacts@gmail.com>
2025-10-07 11:53:32 +0530

    updated ICartService

--- ShoeCartBackend/Services/Interfaces/ICartService.cs
+++ ShoeCartBackend/Services/Interfaces/ICartService.cs
@@ -2,18 +2,13 @@ using ShoeCartBackend.Common;
 
 public interface ICartService
 {
-    // Read cart
     Task<ApiResponse<object>> GetCartForUserAsync(int userId);
 
-    // Add product to cart
     Task<ApiResponse<string>> AddToCartAsync(int userId, int productId, string size, int quantity);
 
-    // Update cart item quantity
     Task<ApiResponse<string>> UpdateCartItemAsync(int userId, int cartItemId, int quantity);
 
-    // Remove single cart item
     Task<ApiResponse<string>> RemoveCartItemAsync(int userId, int cartItemId);
 
-    // Clear all items in cart
     Task<ApiResponse<string>> ClearCartAsync(int userId);
 }
--- ShoeCartBackend/obj/Debug/net8.0/ShoeCartBackend.AssemblyInfo.cs
+++ ShoeCartBackend/obj/Debug/net8.0/ShoeCartBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoeCartBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46742c0f10c4437fae467f9438ae7776bd4c213c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79f97fce9f7b08a045f600b33dd7677a38da951")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoeCartBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoeCartBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
